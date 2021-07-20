f26a97b9aab6ca0a8429ec551b08ab2533da64a0
Yuri Palacio <yurifpalacio@gmail.com>
2021-07-20 20:34:15 -0300

    feature/Devolution Rental
    Criado todo caso de uso e controller para realizar a devolução
    de um carro, tornando o carro disponível e calcula o valor a ser
    pago assim como eventual multa pode atraso na devolução.

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,12 +1,21 @@
 import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCase/createRental/CreateRentalController";
+import { DevolutionRentalController } from "@modules/rentals/useCase/devolutionRental/DevolutionRentalController";
+
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
+
+rentalRoutes.post(
+  "/devolution/:id",
+  ensureAuthenticated,
+  devolutionRentalController.handle
+);
 
 export { rentalRoutes };
