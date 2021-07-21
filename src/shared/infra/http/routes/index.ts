856f1d9e5c0b5bd808b05dca369923956402e304
Yuri Palacio <yurifpalacio@gmail.com>
2021-07-21 17:43:25 -0300

    feature/Reset password
    Desenvolvido processo para reset da senha do usuário.
    É enviado um e-mail para o usuário com o link para reset da senha
    e um token que possui uma validade de 3 horas.
    O layout do e-mail é gerado pelo Handlebars.
    Utilizado o Ethereal para envio do e-mail em ambiente dev.

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { authenticateRoutes } from "./authenticate.routes";
 import { carsRoutes } from "./cars.routes";
 import { categoriesRoutes } from "./categories.routes";
+import { passwordRoutes } from "./password.routes";
 import { rentalRoutes } from "./rental.routes";
 import { specificationRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
@@ -15,5 +16,6 @@ router.use("/specifications", specificationRoutes);
 router.use("/users", usersRoutes);
 router.use("/cars", carsRoutes);
 router.use("/rentals", rentalRoutes);
+router.use("/password", passwordRoutes);
 
 export { router };
