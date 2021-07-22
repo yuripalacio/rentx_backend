5e3fd83e78d52f399b9f305ea13e98bc6065ece3
Yuri Palacio <yurifpalacio@gmail.com>
2021-07-22 08:17:42 -0300

    refactory/Corrigindo testes e aumentando cobertura

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,19 +1,30 @@
+import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
+import { UsersTokensRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory";
+import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
 import { AppError } from "@shared/errors/AppError";
+
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
-import { UsersRepositoryInMemory } from "../../in-memory/UsersRepositoryInMemory";
 import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
 
 let authenticateUserUseCase: AuthenticateUserUseCase;
 let usersRepositoryInMemory: UsersRepositoryInMemory;
+let userTokensRepositoryInMemory: UsersTokensRepositoryInMemory;
+let dateProvider: DayjsDateProvider;
+
 let createUserUseCase: CreateUserUseCase;
 
 describe("Authenticate User", () => {
   beforeEach(() => {
     usersRepositoryInMemory = new UsersRepositoryInMemory();
+    userTokensRepositoryInMemory = new UsersTokensRepositoryInMemory();
+    dateProvider = new DayjsDateProvider();
     authenticateUserUseCase = new AuthenticateUserUseCase(
-      usersRepositoryInMemory
+      usersRepositoryInMemory,
+      userTokensRepositoryInMemory,
+      dateProvider
     );
+
     createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
   });
 
---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.spec.ts
@@ -0,0 +1,65 @@
+import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
+import { UsersTokensRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory";
+import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
+import { MailProviderInMemory } from "@shared/container/providers/MailProvider/in-Memory/MailProviderInMemory";
+import { AppError } from "@shared/errors/AppError";
+import { SendForgotPasswordMailUseCase } from "./SendForgotPasswordMailUseCase";
+
+let sendForgotPasswordMailUseCase: SendForgotPasswordMailUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let dateProvider: DayjsDateProvider;
+let usersTokensRepositoryInMemory: UsersTokensRepositoryInMemory;
+let mailProviderInMemory: MailProviderInMemory;
+
+describe("Send Forgot Mail", () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    dateProvider = new DayjsDateProvider();
+    usersTokensRepositoryInMemory = new UsersTokensRepositoryInMemory();
+    mailProviderInMemory = new MailProviderInMemory();
+    sendForgotPasswordMailUseCase = new SendForgotPasswordMailUseCase(
+      usersRepositoryInMemory,
+      usersTokensRepositoryInMemory,
+      dateProvider,
+      mailProviderInMemory
+    );
+  });
+
+  it("should be able to send a forgot password mail to user", async () => {
+    const sendMail = spyOn(mailProviderInMemory, "sendMail");
+
+    await usersRepositoryInMemory.create({
+      name: "Juan Higgins",
+      username: "juanhiggins",
+      email: "guem@weumo.ac",
+      password: "123456",
+      driver_license: "427680",
+    });
+
+    await sendForgotPasswordMailUseCase.execute("guem@weumo.ac");
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+
+  it("should not be able to send an email if user does not exists", async () => {
+    await expect(
+      sendForgotPasswordMailUseCase.execute("dusi@weor.om")
+    ).rejects.toEqual(new AppError("User does not exist"));
+  });
+
+  it("should be able to create an users token", async () => {
+    const generateTokenMail = spyOn(usersTokensRepositoryInMemory, "create");
+
+    await usersRepositoryInMemory.create({
+      name: "Juan Higgins",
+      username: "juanhiggins",
+      email: "guem@weumo.ac",
+      password: "123456",
+      driver_license: "427680",
+    });
+
+    await sendForgotPasswordMailUseCase.execute("guem@weumo.ac");
+
+    expect(generateTokenMail).toBeCalled();
+  });
+});
